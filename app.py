ec1a05abb97ae2bdb77811890012fc897c89d34e
arsilvaib <arsilvaib@gmail.com>
2026-03-19 17:02:20 -0600

    Primera version: app de desayunos

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def inicio():
+    return render_template('menu.html')
+
+@app.route('/pedido', methods=['POST'])
+def pedido():
+    producto = request.form['producto']
+    cliente = request.form['cliente']
+    direccion = request.form['direccion']
+
+    return f"""
+    <h1>✅ Pedido recibido</h1>
+    <p>Cliente: {cliente}</p>
+    <p>Producto: {producto}</p>
+    <p>Dirección: {direccion}</p>
+    <br>
+    <a href="https://wa.me/5214461414743?text=Hola soy {cliente} y pedí {producto} a la dirección {direccion}">
+        📲 Confirmar por WhatsApp
+    </a>
+    """
+from flask import send_from_directory
+
+@app.route('/manifest.json')
+def manifest():
+    return send_from_directory('.', 'manifest.json')
+
+if __name__ == '__main__':
+    app.run(debug=True)
